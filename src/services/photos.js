a7c6a646f3c5e3553f743e9d7b9a1297ab024df2
hashh <mateusnascimento40@gmail.com>
2021-04-25 17:23:24 -0300

    FEAT: Ajustes pastas e nomes arquivos

--- src/services/photos.js
+++ src/services/photos.js
@@ -2,9 +2,9 @@ import { http } from './config'
 
 export default {
 	index:() => {
-    	return http.get('photos')
+		return http.get('photos')
 	},
 	store:(photo) => {
-    	return http.post('photos', photo)
+		return http.post('photos', photo)
 	}
 }
